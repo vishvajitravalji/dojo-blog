484cacf756050bbcc81b232b2ba587c0560821a6
Vish <vishvajitravalji@gmail.com>
2021-08-03 15:52:31 -0400

    useFetch created

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,18 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
-
-     const [name,setName] = useState('mario');
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
-
-  useEffect(() => {
-    console.log('use effect ran');
-    console.log(name);
-  }, [name])
+ 
+  const{data:blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
+  
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName('vish')}>Change Name</button>
-      <p>{name}</p>
+      { error && <div> {error} </div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
-}
- 
+};
+
 export default Home;
