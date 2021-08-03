0617f0ae178baa3a3a1ff7693d0e9e7cf5f6423b
Vish <vishvajitravalji@gmail.com>
2021-08-03 16:13:35 -0400

    setup routes inside swithes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/create" exact>
+            <Create />
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,6 @@ const Home = () => {
  
   const{data:blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
   
-
   return (
     <div className="home">
       { error && <div> {error} </div>}
